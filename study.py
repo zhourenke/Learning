a47c42fba062f34e2952ba30ea0a37d2cc475297
zhourenke <zhourenke51@gmail.com>
2023-08-25 12:24:52 +0800

    Initial commit

--- study.py
+++ study.py
@@ -3,7 +3,3 @@ head_code.head()
 
 # print("1\r23")
 # print("11\t112\t111\t123")  # 转义字符，包括 \t制表 \n换行 \\打印“ \ ” \'打印“ ' ” \"打印“"” \r回车
-
-print(1 + 1)
-print(1 + 1)
-#  https://github.com/zhourenke/Learning
