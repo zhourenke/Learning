e99508776bd5adb389c568ee37b69812729b1c86
zhourenke <zhourenke51@gmail.com>
2023-08-25 15:59:38 +0800

    Initial commit

---
+++ 猜拳小游戏.py
@@ -0,0 +1,16 @@
+#!/bin/python3.11
+
+import random
+
+players_guess = input("请出拳（石头/剪刀/布）：")
+ais_list = ("石头", "剪刀", "布")
+ais_guess = random.choice(ais_list)
+print(f"*AI出的是{ais_guess}")
+if (players_guess == "石头" and ais_guess == "剪刀") or \
+        (players_guess == "剪刀" and ais_guess == "布") or \
+        (players_guess == "布" and ais_guess == "石头"):
+    print("你赢了")
+elif players_guess == ais_guess:
+    print("平局")
+else:
+    print("你输了")
