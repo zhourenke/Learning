cc401374906084b6580163530325ade7e95df304
zhourenke <zhourenke51@gmail.com>
2023-08-27 16:03:53 +0800

    Initial commit

---
+++ multi.py
@@ -0,0 +1,11 @@
+def multi(num1, num2):
+    """
+
+    :param num1:第一个数
+    :param num2:第二个数=
+    :return:相乘计算结果
+    """
+    return num1 * num2  # 将计算结果返回，进而在def以外调用
+
+
+result = multi(1, 2)
--- study.py
+++ study.py
@@ -4,16 +4,7 @@ head_code.head()  # 输出占位头
 
 # print("1\r23")
 # print("11\t112\t111\t123")  # 转义字符，包括 \t制表 \n换行 \\打印“ \ ” \'打印“ ' ” \"打印“"” \r回车
-
-
-def multi(num1, num2):
-    """
-
-    :param num1:第一个数
-    :param num2:第二个数=
-    :return:相乘计算结果
-    """
-    return num1 * num2  # 将计算结果返回，进而在def以外调用
-
-
-print(multi(1, 2))
+#
+# import multi
+#
+# print(multi.result)  # 模块中的每一个变量都可以引用
