cc4aae76ae304016e728a4df9c5596ba0fb86684
zhourenke <zhourenke51@gmail.com>
2023-09-09 13:51:32 +0800

    Changes

--- additional_expenses.py
+++ additional_expenses.py
@@ -3,12 +3,15 @@
 exp = open(".expenses.txt", "r")
 sources = float(exp.read())
 exp.close()
+
 charge_money = float(input("这次吃饭花了多少？\n> "))
 changes = charge_money - 10
 result = sources + changes
+
 exp = open(".expenses.txt", "w")
 exp.write(f"{result}")
 exp.close()
+
 if result > 0:
     print("还要省%.2f元，继续加油" % result)
 else:
--- study.py
+++ study.py
@@ -10,4 +10,21 @@ head_code.head()  # 输出占位头
 # print(multi.result)  # 模块中的每一个变量都可以引用
 
 num_list = ["111", "222", "333", "444", "555"]
-print(num_list[1])
+print(num_list[1])  # 从列表中取值
+print(num_list.index("111"))  # 从列表中取索引
+num_list[1] = "123"  # 修改列表中的数据
+print(num_list)
+num_list.append("666")  # 追加数据到列表末尾
+print(num_list)
+num_list.insert(3, "123")  # 在指定索引处插入一条数据
+print(num_list)
+num1_list = ["ni hao", "zai ma"]
+num_list.extend(num1_list)  # 将列表 num1_list 连接在列表 num_list 后面
+print(num_list)
+num_list.remove("111")  # 通过字符串搜索从列表中删除指定数据
+print(num_list)
+num_list.pop(3)  # 通过索引搜索从列表中删除指定数据
+print(num_list)
+num_list.pop()  # 删除列表中最后一个数据
+num_list.clear()  # 清空列表中现有的数据
+print(num_list)
