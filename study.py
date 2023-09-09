2928858ea6eb39753cd89a7c00e277931a13871c
zhourenke <zhourenke51@gmail.com>
2023-09-09 16:22:02 +0800

    Changes

--- study.py
+++ study.py
@@ -3,15 +3,22 @@ import head_code
 head_code.head()  # 输出占位头
 
 # print("1\r23")
-# print("11\t112\t111\t123")  # 转义字符，包括 \t制表 \n换行 \\打印“ \ ” \'打印“ ' ” \"打印“"” \r回车
+# print("11\t112\t111\t123")
+# # 转义字符，包括 \t制表 \n换行 \\打印“ \ ” \'打印“ ' ” \"打印“"” \r回车
 #
 # import multi
 #
 # print(multi.result)  # 模块中的每一个变量都可以引用
-
-str_list = ["ni hao", "zai ma", "zai jian"]
+#
+# str_list = ["ni hao", "zai ma", "zai jian"]
 int_list = [1, 2, 234, 654, 4]
-str_list.sort(key=len)  # 以 key 中参数的方式排序
-int_list.sort(reverse=True)  # 反转排序顺序
-print(str_list)
-print(int_list)
+# int_list.reverse()  # 反转列表数据顺序
+# print(int_list)
+# str_list.sort(key=len)  # 以 key 中参数的方式排序
+# int_list.sort(reverse=True)  # 反转排序顺序
+# print(str_list)
+# print(int_list)
+#
+# for num_in in int_list:
+#     # 将 int_list 中的元素依次赋予变量 num_in ，并在每次赋值后执行下方代码
+#     print(num_in)
